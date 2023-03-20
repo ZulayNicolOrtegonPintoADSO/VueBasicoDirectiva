8a3cb2fd6d2a7d62537ba6c8c3948aaaf5f95541
ZulayNicolOrtegónPintoADSO <zulyortegon15@gmail.com>
2023-03-20 16:38:22 -0500

    feat: :art: Directivas 4, 5, 6
     v-if , v-else-if, v-else

--- componentes/my-component.js
+++ componentes/my-component.js
@@ -1,7 +1,11 @@
 export default{
     data(){
         return {
-            centinela: false
+            mensaje:`<em> Apolo </em>` ,
+            mensaje2: `<mark> Artemis </mark>` , 
+            mensaje3:`<u> Sputnik </u>` , 
+            mensaje4: `NO CUMPLE CON LA CONDICIÓN` ,
+            opcion: null,
         }
     }
 }
