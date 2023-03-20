39b3f79956e4f5ddeb2f46a124cb25c28e7343c0
ZulayNicolOrtegónPintoADSO <zulyortegon15@gmail.com>
2023-03-20 16:50:58 -0500

    feat: :art: Directiva 7
     v-bind

--- componentes/my-component.js
+++ componentes/my-component.js
@@ -1,11 +1,11 @@
 export default{
     data(){
         return {
-            mensaje:`<em> Apolo </em>` ,
-            mensaje2: `<mark> Artemis </mark>` , 
-            mensaje3:`<u> Sputnik </u>` , 
-            mensaje4: `NO CUMPLE CON LA CONDICIÓN` ,
-            opcion: null,
+           lista: [
+            "Apolo", 
+            "Artemis" ,
+            "Sputnik"
+           ]
         }
     }
 }
